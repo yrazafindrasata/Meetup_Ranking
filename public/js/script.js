90d1f5c44fb417e35b84704b81b28ee1b30071ab
yoann@DESKTOP-MFUN2T7 <yrazafindrasata@yacast.fr>
2019-09-06 16:26:44 +0200

    fix_top10

--- public/js/script.js
+++ public/js/script.js
@@ -12,6 +12,15 @@ $(document).ready( function () {
     $('#confTab').DataTable()
 } );
 
+$(document).ready( function () {
+    $('#top').DataTable({
+        "sorting": false,
+        "paging": false,
+        "searching": false
+    } );
+} );
+
 function updateTextInput(val) {
     document.getElementById('textInput').value=val;
 }
+
